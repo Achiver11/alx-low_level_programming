2c142c81ff9cc7e353e4ec2c26364fe08a090220
achiver11 <adebayosamuel308@gmail.com>
2022-04-09 15:55:58 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints PROGRAMMING IS LIKE BUILDING A MULTILINGUAL PUZZLE" on standard output
+ *
+ * Return: always 0 if exited properly
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a muiltilingual puzzle");
+
+	return (0);
+}
