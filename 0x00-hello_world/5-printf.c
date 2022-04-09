b0e49247817f48eccd69feb2a08fdaa369d50c38
achiver11 <adebayosamuel308@gmail.com>
2022-04-09 16:10:05 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - use the command printf
+ *
+ * retun: 0 if it exit properly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
