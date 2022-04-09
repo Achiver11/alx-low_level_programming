3bb72ea887a3aa36bf7c43dc9017fc78c0897cb2
achiver11 <adebayosamuel308@gmail.com>
2022-04-09 11:19:46 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	int number;
+for (number = 48; number <= 57; number++)
+{
+	putchar(number);
+if (number == 57)
+{
+	break;
+}
+	putchar(',');
+	putchar(' ');
+}
+	putchar('\n');
+	return (0);
+}
+
