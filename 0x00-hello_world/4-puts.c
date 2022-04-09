f99285e2306871cff3f343e0f056545d40f8eba9
achiver11 <adebayosamuel308@gmail.com>
2022-04-09 16:01:04 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - this program prints PROGRAMMING IS LIKE BUILDING A MULTILINGUAL PUZZLE" on standard output
+ * main - this program print  on standard output
  *
  * Return: always 0 if exited properly
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a muiltilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
