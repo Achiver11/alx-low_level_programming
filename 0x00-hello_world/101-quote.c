690a8a312ec322264f981d5716941b2304ac8b5b
achiver11 <adebayosamuel308@gmail.com>
2022-04-10 09:24:09 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/**
+ * file: 101-quote.c
+ * auth: Brennan
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - to print a quote
+ *
+ * Return: 1 if successful
+ */
+
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - "
+"Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}	
