b670e15495e0994b558b96cc343685fcc1bfa814
achiver11 <adebayosamuel308@gmail.com>
2022-04-10 14:10:09 -0700

    extra 10, hellpworld

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio>
+
+/**
+ * main- entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int digit1 = 0;
+	int digit2;
+
+	while (digit1 < 10)
+{
+	for (digit2 = 0; digit2 < 10; digit2++){
+	putchar((digit1 % 10) + '0');
+
+	putchar((digit2 % 10) + '0')
+	
+	if (digit1 == 9 && digit2 == 9)
+{
+	break;
+}
+	else
+{
+	putchar(44);
+
+	putchar(32);
+}
+}
+	digit1++;
+}
+	putchar('\n');
+
+	return 0
+}	
