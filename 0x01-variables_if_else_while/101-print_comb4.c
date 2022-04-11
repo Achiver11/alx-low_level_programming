16352494b33c5990407931e36cf5206f8fc7f9b3
achiver11 <adebayosamuel308@gmail.com>
2022-04-11 01:00:53 -0700

    e11,12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - to print all possible combo of 3 digits
+ *
+ * Return: 0 is successful
+ */
+
+int main(void)
+{
+	int digit1 = 0;
+	int digit2, digit3;
+
+	while (digit 1 < 8)
+{
+	for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+{
+	for digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
+	putchar((digit1 % 10) + '0');
+	putchar((digit2 % 10) + '0');
+	putchar((digit3 % 10) + '0');
+
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+{
+	break;
+}
+else
+	putchar(44);
+	putchar(32);
+}
+}
+}
+	digit1++;
+}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - to print all possible combo of two two-digit numbers
+ *
+ * Return : 0 if successful
+ */
+
+int main(void)
+{
+	int digit1 = 0;
+	int digit2, digit3, digit4;
+
+	while (digit1 < 10)
+{
+	for (digit2 = 0; digit2 < 9; digit2++)
+{
+	for (digit3 = digit1; digit3 < 10; digit3++)
+{
+	for (digit4 = digit2 + 1; digit4 < 10; digit4++)
+{
+	puchar((digit1 % 10) + '0');
+	putchar((digit2 % 10) + '0');
+	putchar(32);
+	putchar((digit3 % 10) + '0');
+	putchar((digit4 % 10) + '0');
+
+if (digit1 == 9 && digit2 == 9 && digit3 == 9 && digit4 == 9)
+{
+	break;
+}
+else
+{
+	putchar(44);
+}
+}
+}
+}
+	digit1++;
+}
+	putchar('\n');
+
+	return (0);
+}
