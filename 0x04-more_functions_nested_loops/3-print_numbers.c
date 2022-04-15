7a6228e6063ce7386102fd4321b8257bba2e7156
achiver11 <adebayosamuel308@gmail.com>
2022-04-15 06:35:32 -0700

    t3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * pribt_numbers - function yhat prints numbers
+ * 0 to 9
+ * Return : nothing
+ */
+void print_numbers(void)
+{
+	int number = 48;
+
+	for (number = 48; number < 58; number++)
+	
+{
+	_putchar(number);
+}
+	_putchar(10);
+}
